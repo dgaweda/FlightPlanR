d646dbab0be24dd1bbad2f5e9a05a0fd20dc1394
Darek Gawęda <d.gaweda@npc.pl>
2023-01-09 20:16:16 +0100

    Init FlightPlan Api Proj

---
+++ FlightPlanApi/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(config =>
+{
+    config
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+});
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.Run();
